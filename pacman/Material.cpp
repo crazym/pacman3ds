a8a8356495d59b319eda52a9b9f624be0d6f55ee
Bronson.Zgeb <Bronson.Zgeb@c5c58454-be73-59b6-f510-02fc288a1589>
2010-08-05 16:10:02 +0000

    Missed a couple files.

---
+++ pacman/Material.cpp
@@ -0,0 +1,43 @@
+/*
+ *  Material.cpp
+ *  Pacman3D
+ *
+ *  Created by bronson on 10-08-05.
+ *  Copyright 2010 Concordia University. All rights reserved.
+ *
+ */
+#include "Material.h"
+
+GLfloat no_mat[]            = { 0.0, 0.0, 0.0, 1.0 };
+GLfloat mat_ambient[]       = { 0.5, 0.5, 0.5, 1.0 };
+GLfloat mat_ambient_color[] = { 0.8, 0.8, 0.2, 1.0 };
+GLfloat mat_diffuse[]       = { 0.1, 0.5, 0.8, 1.0 };
+GLfloat mat_specular[]      = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat no_specular[]       = { 0.0, 0.0, 0.0, 1.0 };
+GLfloat no_shininess[]      = { 0.0 };
+GLfloat low_shininess[]     = { 5.0 };
+GLfloat high_shininess[]    = { 100.0 };
+GLfloat mat_emission[]      = { 0.3, 0.2, 0.2, 0.0 };
+
+GLfloat pacman_body_ambient[] = { 1.0, 1.0, 0.0, 1.0 };
+GLfloat pacman_body_diffuse[] = { 1.0, 1.0, 0.0, 1.0 };
+GLfloat pacman_palate[]       = { 1.0, 0.0, 0.0, 1.0 };
+GLfloat pacman_retina[]       = { 0.0, 0.0, 0.0, 1.0 };
+GLfloat pacman_pupil[]        = { 1.0, 1.0, 1.0, 1.0 };
+
+GLfloat pacman_palate_jacko[] = { 0.0, 0.0, 0.0, 1.0 };
+GLfloat pacman_pupil_jacko[]  = { 0.0, 0.0, 0.0, 1.0 };
+
+
+
+GLfloat ghost_body[]  = { 0.75, 0.75, 0.75, 0.65 };
+GLfloat ghost_white[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat ghost_pupil[] = { 0.0, 0.0, 1.0, 1.0 };
+
+GLfloat group_number[] = { 0.0, 0.0, 0.0, 1.0 };
+
+GLfloat lamp_emission_on[]     = { 0.4, 0.4, 0.0, 0.0};
+GLfloat lamp_emission_off[]    = { 0.0, 0.0, 0.0, 0.0};
+GLfloat lamp_specular_on[]     = { 1.0, 1.0, 0.0, 1.0 };
+GLfloat lamp_ambient_diffuse[] = { 1.0, 1.0, 0.0, 1.0 };
+GLfloat pole_ambient_diffuse[] = { 0.6, 0.6, 0.6, 1.0 };
---
+++ pacman/textures.h
@@ -0,0 +1,38 @@
+#ifndef textures_h__
+#define textures_h__
+
+
+
+
+#include <fstream>  
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else 
+#include <windows.h> 
+#include <GL/glut.h>
+#endif
+
+#ifndef GL_EXT_texture_cube_map
+# define GL_NORMAL_MAP_EXT                   0x8511
+# define GL_REFLECTION_MAP_EXT               0x8512
+# define GL_TEXTURE_CUBE_MAP_EXT             0x8513
+# define GL_TEXTURE_BINDING_CUBE_MAP_EXT     0x8514
+# define GL_TEXTURE_CUBE_MAP_POSITIVE_X_EXT  0x8515
+# define GL_TEXTURE_CUBE_MAP_NEGATIVE_X_EXT  0x8516
+# define GL_TEXTURE_CUBE_MAP_POSITIVE_Y_EXT  0x8517
+# define GL_TEXTURE_CUBE_MAP_NEGATIVE_Y_EXT  0x8518
+# define GL_TEXTURE_CUBE_MAP_POSITIVE_Z_EXT  0x8519
+# define GL_TEXTURE_CUBE_MAP_NEGATIVE_Z_EXT  0x851A
+# define GL_PROXY_TEXTURE_CUBE_MAP_EXT       0x851B
+# define GL_MAX_CUBE_MAP_TEXTURE_SIZE_EXT    0x851C
+#endif
+
+GLuint LoadTextureRAW( const char * filename, int wrap, int w, int h ) ;
+
+
+
+#endif textures_h__
+
+
+
