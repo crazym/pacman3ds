edfd0ea4185fb70c1b61a2896c9784781bd6fbce
Bronson.Zgeb <Bronson.Zgeb@c5c58454-be73-59b6-f510-02fc288a1589>
2010-08-05 16:15:49 +0000

    Updated headers for linux.

--- trunk/pacman/Cube.h
+++ trunk/pacman/Cube.h
@@ -10,11 +10,17 @@
 #ifndef _CUBE_H_
 #define _CUBE_H_
 
-#ifdef __APPLE__
+#ifdef __APPLE__ /* OS X */
 #include <cstdlib>
 #include <GLUT/glut.h>
-#else 
+#elif defined(__linux__) /* LINUX */
 #include <stdlib.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#else /* WINDOWS */
+#include <stdlib.h>
+#include <windows.h>
 #include <GL/glut.h>
 #endif
 
