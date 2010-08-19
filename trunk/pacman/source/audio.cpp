0c2fc639ba59e638eefaa6348f51ea1871cf7c66
Bronson.Zgeb <Bronson.Zgeb@c5c58454-be73-59b6-f510-02fc288a1589>
2010-08-19 01:06:02 +0000

    Cleaned up audio a little.

--- trunk/pacman/source/audio.cpp
+++ trunk/pacman/source/audio.cpp
@@ -37,22 +37,17 @@
 
 using namespace irrklang;
 
-
-ISoundEngine* intro_audio = createIrrKlangDevice();
-ISoundEngine* game_ended_audio = createIrrKlangDevice();
-ISoundEngine* pacman_eating_audio = createIrrKlangDevice();
-ISoundEngine* pacman_dying_audio = createIrrKlangDevice();
-ISoundEngine* frenzy_audio = createIrrKlangDevice();
-
+ISoundEngine* musicSoundEngine = createIrrKlangDevice();
+ISoundEngine* soundEffectsEngine = createIrrKlangDevice();
  
 void play_intro_audio(){
 
-	if (!intro_audio)
+	if (!musicSoundEngine)
 	{
 		return; // error starting up the engine
 	}
 
-	intro_audio->play2D(startLevelSound, false);
+    musicSoundEngine->play2D(startLevelSound, false);
 
 	return;
 }
@@ -60,36 +55,36 @@ void play_intro_audio(){
 
 void play_pacman_eating_audio(){
 
-	if (!pacman_eating_audio)
+	if (!soundEffectsEngine)
 	{
 		return; // error starting up the engine
 	}
 
-	pacman_eating_audio->play2D(pacmanEatingSound, false);
-
+    soundEffectsEngine->play2D(pacmanEatingSound, false);
+    
 	return;
 }
 
 void play_pacman_dying_audio(){
 
-	if (!pacman_dying_audio)
+	if (!soundEffectsEngine)
 	{
 		return; // error starting up the engine
 	}
 
-	pacman_dying_audio->play2D(pacmanDyingSound, false);
+    soundEffectsEngine->play2D(pacmanDyingSound, false);
 
 	return;
 }
 
 void play_game_ended_audio(){
 
-	if (!game_ended_audio)
+	if (!soundEffectsEngine)
 	{
 		return; // error starting up the engine
 	}
 
-	game_ended_audio->play2D(gameOverSound, false);
+    soundEffectsEngine->play2D(gameOverSound, false);
 
 	return;
 }
@@ -97,37 +92,37 @@ void play_game_ended_audio(){
 
 void play_frenzy_audio(){
 
-	if (!frenzy_audio)
+	if (!musicSoundEngine)
 	{
 		return; // error starting up the engine
 	}
 
-	frenzy_audio->play2D(frenzySound, false);
+    musicSoundEngine->play2D(frenzySound, false);
 
 	return;
 }
 
 void stop_intro_audio(){
 
-	intro_audio->stopAllSounds(); 
+	musicSoundEngine->stopAllSounds(); 
 }
 
 void stop_pacman_eating_audio(){
 
-	pacman_eating_audio->stopAllSounds(); 
+	soundEffectsEngine->stopAllSounds(); 
 }
 
 void stop_pacman_dying_audio(){
 
-	pacman_dying_audio->stopAllSounds(); 
+	soundEffectsEngine->stopAllSounds(); 
 }
 
 void stop_game_ended_audio(){
 
-	game_ended_audio->stopAllSounds(); 
+	soundEffectsEngine->stopAllSounds(); 
 }
 
 void stop_frenzy_audio(){
 
-	frenzy_audio->stopAllSounds(); 
+	musicSoundEngine->stopAllSounds(); 
 }
